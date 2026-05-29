d8999fcb9a59f3d8463503d2dbc9b34fa2da0095
Константин Тимошенко <exponenta@Air-Konstantin.lan>
2026-05-29 16:05:52 +0300

    build with new flags

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 MUPDF_DIR=libmupdf
 EMSDK_DIR=/opt/emsdk
 
-MUPDF_OPTS="-Os -DTOFU -DTOFU_CJK_EXT -DFZ_ENABLE_XPS=0 -DFZ_ENABLE_SVG=0 -DFZ_ENABLE_CBZ=0 -DFZ_ENABLE_IMG=0 -DFZ_ENABLE_HTML=0 -DFZ_ENABLE_EPUB=0 -DFZ_ENABLE_JS=0 -DFZ_ENABLE_OCR_OUTPUT=0 -DFZ_ENABLE_DOCX_OUTPUT=0 -DFZ_ENABLE_ODT_OUTPUT=0 -DMEMENTO_STACKTRACE_METHOD=0"
+MUPDF_OPTS="-Os -mno-nontrapping-fptoint -fwasm-exceptions -sSUPPORT_LONGJMP=wasm -DTOFU -DTOFU_CJK_EXT -DFZ_ENABLE_XPS=0 -DFZ_ENABLE_SVG=0 -DFZ_ENABLE_CBZ=0 -DFZ_ENABLE_IMG=0 -DFZ_ENABLE_HTML=0 -DFZ_ENABLE_EPUB=0 -DFZ_ENABLE_JS=0 -DFZ_ENABLE_OCR_OUTPUT=0 -DFZ_ENABLE_DOCX_OUTPUT=0 -DFZ_ENABLE_ODT_OUTPUT=0 -DMEMENTO_STACKTRACE_METHOD=0"
 
 export EMSDK_QUIET=1
 source $EMSDK_DIR/emsdk_env.sh
@@ -17,20 +17,27 @@ echo
 
 echo BUILDING MUPDF WASM
 mkdir -p dist
-emcc -o dist/mupdf-wasm.js -I $MUPDF_DIR/include src/mupdf.c \
-	--no-entry \
-	-sABORTING_MALLOC=0 \
-	-sALLOW_MEMORY_GROWTH=1 \
-	-sMAXIMUM_MEMORY=4GB \
-	-sNODEJS_CATCH_EXIT=0 \
-	-sMODULARIZE=1 \
-	-sEXPORT_ES6=1 \
-	-sEXPORT_NAME='"libmupdf_wasm"' \
-	-sEXPORTED_RUNTIME_METHODS='["ccall","UTF8ToString","lengthBytesUTF8","stringToUTF8","HEAP8","HEAPU8","HEAPU32","HEAPF32"]' \
-	 $MUPDF_DIR/build/wasm/$BUILD/libmupdf.a \
-	 $MUPDF_DIR/build/wasm/$BUILD/libmupdf-third.a
+emcc -o dist/mupdf-wasm.js -I $MUPDF_DIR/include \
+   -Os -g2 \
+   -mno-nontrapping-fptoint \
+   -fwasm-exceptions \
+   --no-entry \
+   -sSUPPORT_LONGJMP=wasm \
+   -sABORTING_MALLOC=0 \
+   -sALLOW_MEMORY_GROWTH=1 \
+   -sMAXIMUM_MEMORY=4GB \
+   -sNODEJS_CATCH_EXIT=0 \
+   -sMODULARIZE=1 \
+   -sEXPORT_ES6=1 \
+   -sEXPORT_NAME='"libmupdf_wasm"' \
+   -sTEXTDECODER=2 \
+   -sFILESYSTEM=0 \
+   -sEXPORTED_RUNTIME_METHODS='["ccall","UTF8ToString","lengthBytesUTF8","stringToUTF8","HEAP8","HEAPU8","HEAPU32","HEAPF32"]' \
+   src/mupdf.c \
+    $MUPDF_DIR/build/wasm/$BUILD/libmupdf.a \
+    $MUPDF_DIR/build/wasm/$BUILD/libmupdf-third.a
 echo
 
-echo BUILDING TYPESCRIPT
-cat src/mupdf.c | sed '/#include/d' | emcc -E - | node src/gen-wasm-type.js > src/mupdf-wasm.d.ts
-npx tsc -p .
+# echo BUILDING TYPESCRIPT
+# cat src/mupdf.c | sed '/#include/d' | emcc -E - | node src/gen-wasm-type.js > src/mupdf-wasm.d.ts
+# npx tsc -p .
